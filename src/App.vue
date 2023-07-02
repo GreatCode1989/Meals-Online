84286bac54baca99be459c4a85fd5643805f3070
Пользователь <wef666wef6@gmail.com>
2023-07-02 18:57:06 +0300

    l

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="bg-gray-100 h-full">
+    <header class="bg-white shadow h-16 flex justify-between">
+      <router-link :to="{ name: 'home' }"> Home </router-link>
+      <div>
+        <router-link to="/"> Searcch Meals </router-link>
+        <router-link to="/"> Meals by Letter </router-link>
+        <router-link to="/"> Meals by Ingredient </router-link>
+      </div>
+    </header>
+
+    <main>
+      <router-view></router-view>
+    </main>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
---
+++ src/views/Home.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="flex flex-col p-8">
+    <input
+      type="text"
+      class="rounded border-2 border-gray-200 w-full"
+      placeholder="Search for Meals"
+    />
+
+    <div class="flex justify-center gap-2 mt-2">
+      <router-link
+        v-for="letter of letters"
+        :key="letter"
+        :to="{ name: 'byLetter', params: { letter } }"
+      >
+        {{ letter }}
+      </router-link>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { computed, onMounted } from "vue";
+import store from "../store";
+import { ref } from "vue";
+import axiosClient from "../AxiosClient";
+
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+const ingredients = ref([]);
+
+onMounted(async () => {
+  const response = await axiosClient.get("/list.php?i=list");
+  console.log(response.data);
+  ingredients.value = response.data;
+});
+</script>
